7e9d0dd79aeb7563709e606f3005b3b58373ecbe
Sheung <shegunmb@gmail.com>
2026-02-06 11:39:54 +0100

    💄 Update color if crypto 24 hour change  is positive or negative

--- src/features/cryptocurrencies/CryptocurrencyList.tsx
+++ src/features/cryptocurrencies/CryptocurrencyList.tsx
@@ -22,16 +22,21 @@ export default async function CryptocurrencyList() {
           </tr>
         </thead>
         <tbody>
-          {cryptoItems.map((crypto, i) => (
-            <tr key={crypto.name}>
-              <th>{i + 1}</th>
-              <td>
-                <Link href={`coins/${crypto.name}`}>{crypto.name} </Link>
-              </td>
-              <td>{crypto.priceUSD}</td>
-              <td>{crypto.usd24hChange}</td>
-            </tr>
-          ))}
+          {cryptoItems.map((crypto, i) => {
+            const colorFor24hChange =
+              crypto.usd24hChange > 0 ? "text-success" : "text-error";
+            const COIN_DETAILS_URL = `coins/${crypto.name}`;
+            return (
+              <tr key={crypto.name}>
+                <th>{i + 1}</th>
+                <td>
+                  <Link href={COIN_DETAILS_URL}>{crypto.name} </Link>
+                </td>
+                <td>{crypto.priceUSD}</td>
+                <td className={colorFor24hChange}>{crypto.usd24hChange}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
