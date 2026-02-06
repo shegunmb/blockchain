358abd847fc3ff3d0380d25074c8774288b0e3a2
Sheung <shegunmb@gmail.com>
2026-02-06 11:35:01 +0100

    ♻️ Create  utils functions to check API key, get options and check api response

--- src/api/cryptocurrencies/coinGecko/index.ts
+++ src/api/cryptocurrencies/coinGecko/index.ts
@@ -1,31 +1,19 @@
-import { config } from "@config";
 import type {
+  CryptocurrencyAPI,
   CryptocurrencyItem,
   CryptocurrencyName,
 } from "@/domain/cryptocurrencies/types";
 import { resolveCryptocurrencies } from "../resolver";
-
-const COIN_GECKO_BASE_URL = config.coinGeckoBaseUrl;
-const COIN_GECKO_API_KEY: string = process.env.COIN_GECKO_API_KEY || "";
+import { fetchCoinGecko, getOptions } from "./utils";
 
 export function fetchCryptocurrenciesFromCoinGecko(
   cryptoList: CryptocurrencyName[],
 ): Promise<CryptocurrencyItem[]> {
-  if (!COIN_GECKO_API_KEY) {
-    throw new Error("COIN_GECKO_API_KEY environment variable is not set");
-  }
-
-  const options: RequestInit = {
-    method: "GET",
-    cache: "no-store",
-    headers: { x_cg_demo_api_key: COIN_GECKO_API_KEY },
-  };
+  const options = getOptions();
 
   const cryptosToFetch = cryptoList.join(",");
-  return fetch(
-    `${COIN_GECKO_BASE_URL}/simple/price?vs_currencies=usd&ids=${cryptosToFetch}&include_24hr_change=true&precision=2`,
+  return fetchCoinGecko<Record<string, CryptocurrencyAPI>>(
+    `/simple/price?vs_currencies=usd&ids=${cryptosToFetch}&include_24hr_change=true&precision=2`,
     options,
-  )
-    .then((res) => res.json())
-    .then(resolveCryptocurrencies);
+  ).then(resolveCryptocurrencies);
 }
